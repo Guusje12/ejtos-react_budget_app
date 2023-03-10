48482a2f5076405840663543e852ed88c773fbfc
Your Name <you@example.com>
2023-03-10 08:33:12 -0500

    Final exercise task 2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,36 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    // if(budget > 1000) {
+    //     alert("Budget cannot exceed  £20,000");
+    //     return;
+    // }
+
+    const setBudget = (value) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value
+        });
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                value={budget}
+                style={{ marginLeft: '2rem', size: 10 }}
+                step = '10'
+                onChange={(event) => setBudget(event.target.value)}>
+            </input>
         </div>
     );
 };
+
+
+
 export default Budget;
